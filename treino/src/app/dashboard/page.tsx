09e4633223e2f8934de8a7b9c3d6ed62d4f827df
jpedrobacker <jpedrobacker@outlook.com>
2024-12-01 19:30:34 -0300

    done 2

--- treino/src/app/dashboard/page.tsx
+++ treino/src/app/dashboard/page.tsx
@@ -2,8 +2,7 @@
 import React from "react";
 
 const Dashboard: React.FC = () => {
-	const statuses: number[] = [200, 404, 500, 403, 418];
-
+	const statusARR: number[] = [200, 404, 500, 403, 418];
 	const handleLogout = () => {
 		window.location.href = "/login";
 	};
@@ -12,7 +11,7 @@ const Dashboard: React.FC = () => {
 		<div className="p-6 bg-gray-800 min-h-screen">
 			<h1 className="text-2xl font-semibold text-sky-600 mb-6">Dashboard</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-				{statuses.map((status) => ( <div key={status} className="bg-white shadow-md p-4">
+				{statusARR.map((status) => ( <div key={status} className="bg-white shadow-md p-4">
 						<img src={`https://http.dog/${status}.jpg`} alt={`HTTP Dog ${status}`} className="w-full h-auto"/>
 						<img src={`https://http.cat/${status}.jpg`} alt={`HTTP Cat ${status}`} className="w-full h-automt-4"/>
 					</div>))}
